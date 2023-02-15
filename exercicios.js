b9ee4e82fad8fa8d77d80d0972650699e329837b
henriquelbk <loubackhenrique123@gmail.com>
2023-02-14 22:06:03 -0300

    att 1

--- exercicios.js
+++ exercicios.js
@@ -19,44 +19,69 @@ function imprimeMensagem() {
 // EXERCÍCIO 01
 function calculaAreaRetangulo() {
   // implemente sua lógica aqui
+  const altura = Number(prompt('Didite a altura do retângulo:'))
+  const largura = Number(prompt('Didite a largura do retângulo:'))
 
+  console.log(altura * largura)
 }
 
 // EXERCÍCIO 02
 function imprimeIdade() {
   // implemente sua lógica aqui
-
+  const anoAtual = Number(prompt('Qual é o ano atual?'))
+  const anoDeNascimento = Number(prompt('Em que ano você nasceu?'))
+  
+  console.log(anoAtual - anoDeNascimento)
 }
 
 // EXERCÍCIO 03
 function calculaIMC(peso, altura) {
   // implemente sua lógica aqui
-
+  const pesoUsuario = peso
+  const alturaUsuario = altura
+  const imc = pesoUsuario / (alturaUsuario * alturaUsuario)
+  return imc
 }
 
 // EXERCÍCIO 04
 function imprimeInformacoesUsuario() {
   // implemente sua lógica aqui
   // "Meu nome é NOME, tenho IDADE anos, e o meu email é EMAIL."
-
+  const seuNome = prompt('Qual é o seu nome?')
+  const suaIdade = Number(prompt('Qual é a sua idade?'))
+  const seuEmail = prompt('Qual é o seu e-mail?')
+  
+  console.log(`Meu nome é ${seuNome}, tenho ${suaIdade} anos, e o meu email é ${seuEmail}.`)
 }
+
 
 // EXERCÍCIO 05
 function imprimeTresCoresFavoritas() {
   // implemente sua lógica aqui
-
+  const corFavorita1 = prompt('Qual é a sua cor favorita?')
+  const corFavorita2 = prompt('Qual é a sua segunda cor favorita?')
+  const corFavorita3 = prompt('Qual é a sua terceira cor favorita?')
+  const coresFavoritas = [corFavorita1, corFavorita2, corFavorita3]
+  
+  console.log(coresFavoritas)
 }
 
 // EXERCÍCIO 06
 function retornaStringEmMaiuscula(string) {
   // implemente sua lógica aqui
-
+  const string3 = string
+  const string4 = string3.toUpperCase()
+  return string4
 }
 
 // EXERCÍCIO 07
 function calculaIngressosEspetaculo(custo, valorIngresso) {
   // implemente sua lógica aqui
+  const custoTeatro = custo
+  const valorDoIngresso = valorIngresso
+  const quantIngressos = custoTeatro / valorDoIngresso
 
+  return quantIngressos
 }
 
 // EXERCÍCIO 08
