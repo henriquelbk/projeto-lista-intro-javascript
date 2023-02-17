1b77d04163ad841fa470731ba1ab41e6d6df4224
henriquelbk <loubackhenrique123@gmail.com>
2023-02-16 22:15:12 -0300

    quase finalizado

--- exercicios.js
+++ exercicios.js
@@ -87,47 +87,82 @@ function calculaIngressosEspetaculo(custo, valorIngresso) {
 // EXERCÍCIO 08
 function checaStringsMesmoTamanho(string1, string2) {
   // implemente sua lógica aqui
+  const minhaString = string1
+  const suaString = string2
+  const tamanhoDaMinha = minhaString.length
+  const tamanhoDaSua = suaString.length
 
+  return tamanhoDaMinha === tamanhoDaSua
 }
 
 // EXERCÍCIO 09
 function retornaPrimeiroElemento(array) {
   // implemente sua lógica aqui
+  const array1 = array
+  const primeiroItem = array1[0]
 
+  return primeiroItem
 }
 
 // EXERCÍCIO 10
 function retornaUltimoElemento(array) {
   // implemente sua lógica aqui
+  const array2 = array
+  const ultimoItem = array2[array.length -1]
 
+  return ultimoItem
 }
 
 // EXERCÍCIO 11
 function trocaPrimeiroEUltimo(array) {
   // implemente sua lógica aqui
+  const array3 = array
+  const primeiraPosicao = array3[0]
+  const ultimaPosicao = array3[array3.length -1]
+  
+  array3[array3.length -1] = primeiraPosicao
+  array3 [0] = ultimaPosicao
 
+  return array3
 }
 
 // EXERCÍCIO 12
 function checaIgualdadeDesconsiderandoCase(string1, string2) {
   // implemente sua lógica aqui
+const string5 = string1
+const string6 = string2
 
+return string5.toUpperCase() === string6.toUpperCase()
 }
 
 // EXERCÍCIO 13
 function checaRenovacaoRG() {
   // implemente sua lógica aqui
+  const anoAtual = Number(prompt('Qual o ano atual?'))
+  const anoNasc = Number(prompt('Em qual ano fulano nasceu?'))
+  const anoRG = Number(prompt('Em qual ano o RG de fulano foi emitido?'))
 
+  const renovacao20 = anoAtual - anoNasc <= 20 && anoAtual - anoRG >= 5
+  const renovacao21a50 = anoAtual - anoNasc > 20 <= 50 && anoAtual - anoRG >= 10
+  const renovacao51Mais = anoAtual - anoNasc > 50 && anoAtual - anoRG >= 15
+
+  console.log(renovacao20 || renovacao21a50 || renovacao51Mais)
 }
 
 // EXERCÍCIO 14
 function checaAnoBissexto(ano) {
   // implemente sua lógica aqui
+  const anoBissexto = ano
 
+  return anoBissexto % 400 && anoBissexto % 4 && anoBissexto % 100 !== 0
 }
 
 // EXERCÍCIO 15
 function checaValidadeInscricaoLabenu() {
   // implemente sua lógica aqui
+  const maioridade = Boolean(prompt('Tem mais de 18 anos?'))
+  const EM = Boolean(prompt('Tem Ensino Médio completo?'))
+  const disponibilidade = Boolean(prompt('Tem disponibilidade exclusiva?'))
 
+  console.log(maioridade && EM && disponibilidade)
 }
